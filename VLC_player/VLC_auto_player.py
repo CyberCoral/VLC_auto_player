6cff48f9c504f9a42c9c6980d153a3a5662dd8cd
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2023-12-16 19:34:33 +0100

    Added variable cmd_gen to file_auto_player()
    Added variable cmd_gen so alt-f4 is not mandatory, but case dependant.

--- VLC_player/VLC_auto_player.py
+++ VLC_player/VLC_auto_player.py
@@ -14,19 +14,13 @@
 ### Download Python3: https://www.python.org/downloads/
 ###
 
-##  Steps to use VLC_auto_player():
+##  Steps to use this:
 ##    -Step 1: Open VLC
 ##    -Step 2: Click on the VLC window.
 ##    -Step 3: Click on IDLE/Shell
 ##    -Step 4: Enter VLC_auto_player(settings you want to use)
 ##              VLC_auto_player(loops = True || False, txt_files = [...], right_s = 1 (don't change))
 ##    -Step 5: Enjoy :D
-
-## Steps to use file_auto_player()
-##    -Step 1: Put your files in VLC_player folder.
-##    -Step 2: Put files=(<files>) in file_auto_player()
-##    -Step 3: Press Enter
-##    -Step 4: Enjoy :·D
 
 ###
 ### Import modules.
@@ -191,14 +185,14 @@ def create_playlist(txt_file: str = "playlist_0.txt", *songs):
             songs[i] = songs[i].replace("https://","http://")
             f.write(f"{songs[i]}\n")
 
-def file_auto_player(loops: bool = True,*,files: list = [""]):
+def file_auto_player(loops: bool = True,cmd_gen: bool = True,*,files: list = [""]):
     '''
     It auto plays any file
     (mp3, mp4, avi...) on VLC,
     and it works for Windows 
     and Linux (WIP).
     '''
-    AEC.AutomatedErrorTypeFinder([loops,files],["1","00001"])
+    AEC.AutomatedErrorTypeFinder([loops,cmd_gen,files],["1","1","00001"])
     AEC.AutomatedConditionCheck([[isinstance(files[i], str) for i in range(len(files))]],["var.count(False) == 0"],[2])
 
     os.chdir("/")
@@ -206,13 +200,14 @@ def file_auto_player(loops: bool = True,*,files: list = [""]):
     folder = folder[:re.search("VLC_player",folder).end()]+"/"
     os.chdir(folder)
 
-    def windows_part(files):
+    def windows_part(files, cmd_gen: bool = True):
         for i in range(len(files)):
             try:
                 os.system(f'start /b cmd /k "%PROGRAMFILES%/VideoLAN/VLC/vlc.exe" {files[i]}')
-                time.sleep(0.5)
+                time.sleep(1)
                 hold_and_press(["alt","tab"],[])
-                pyautogui.hotkey("alt","f4")
+                if cmd_gen == True:
+                    pyautogui.hotkey("alt","f4")
                 
                 image=pyscreenshot.grab(bbox=(0, 0, 300, 45))
                 time.sleep(2)
@@ -252,10 +247,10 @@ def file_auto_player(loops: bool = True,*,files: list = [""]):
     if loops == True:
         if platform.system() == "Windows":
             if loops == True:
-                windows_part(files)
+                windows_part(files,cmd_gen)
             else:
                 while True:
-                    windows_part(files)
+                    windows_part(files,cmd_gen)
         
     elif platform.system() == "Linux":
         if loops == True:

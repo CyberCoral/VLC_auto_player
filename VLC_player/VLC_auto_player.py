84d622861f9e0b2ad743b272f89821fc6393d649
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2023-12-16 19:21:19 +0100

    Added file_auto_player()
    Added new function file_auto_player(), which the user can use to play their favorite files (mp3, mp4, avi...) that they have on VLC_player.

--- VLC_player/VLC_auto_player.py
+++ VLC_player/VLC_auto_player.py
@@ -14,25 +14,31 @@
 ### Download Python3: https://www.python.org/downloads/
 ###
 
-##  Steps to use this:
-##    -Step 1: Open VLC at fullscreen.
+##  Steps to use VLC_auto_player():
+##    -Step 1: Open VLC
 ##    -Step 2: Click on the VLC window.
-##    -Step 3: Click on IDLE/Shell/IDE.
+##    -Step 3: Click on IDLE/Shell
 ##    -Step 4: Enter VLC_auto_player(settings you want to use)
 ##              VLC_auto_player(loops = True || False, txt_files = [...], right_s = 1 (don't change))
 ##    -Step 5: Enjoy :D
+
+## Steps to use file_auto_player()
+##    -Step 1: Put your files in VLC_player folder.
+##    -Step 2: Put files=(<files>) in file_auto_player()
+##    -Step 3: Press Enter
+##    -Step 4: Enjoy :·D
 
 ###
 ### Import modules.
 ###
 
 # built-in
-import os, sys, subprocess
+import os, sys, subprocess, platform, re
 
 modules = ["pyautogui","pyperclip","pyscreenshot","keyboard","OpenCV-Python","numpy","mss"]
 
-for i in modules:
-    subprocess.run(["python","-m","pip","install",f"{i}"])
+##for i in modules:
+##    subprocess.run(["python","-m","pip","install",f"{i}"])
     
 import pyautogui, pyperclip
 import pyscreenshot
@@ -77,7 +83,7 @@ def hold_and_press(hold: list = ["shift"], press: list = ["v"]):
     pyautogui.press() but good.
     '''
     AEC.AutomatedErrorTypeFinder([hold, press],["00001","00001"])
-    AEC.AutomatedConditionCheck([hold, press],["len(var) > 0","len(var) > 0"],[2,2])
+    AEC.AutomatedConditionCheck([hold, press],["len(var) >= 0","len(var) >= 0"],[2,2])
 
     for i in hold:
         pyautogui.keyDown(i)
@@ -88,9 +94,9 @@ def hold_and_press(hold: list = ["shift"], press: list = ["v"]):
         pyautogui.keyUp(i)
 
 ##  Steps to use this:
-##    -Step 1: Open VLC at fullscreen.
+##    -Step 1: Open VLC
 ##    -Step 2: Click on the VLC window.
-##    -Step 3: Click on IDLE/Shell/IDE.
+##    -Step 3: Click on IDLE/Shell
 ##    -Step 4: Enter VLC_auto_player(...)
 ##    -Step 5: Enjoy :D
         
@@ -185,4 +191,83 @@ def create_playlist(txt_file: str = "playlist_0.txt", *songs):
             songs[i] = songs[i].replace("https://","http://")
             f.write(f"{songs[i]}\n")
 
-    
+def file_auto_player(loops: bool = True,*,files: list = [""]):
+    '''
+    It auto plays any file
+    (mp3, mp4, avi...) on VLC,
+    and it works for Windows 
+    and Linux (WIP).
+    '''
+    AEC.AutomatedErrorTypeFinder([loops,files],["1","00001"])
+    AEC.AutomatedConditionCheck([[isinstance(files[i], str) for i in range(len(files))]],["var.count(False) == 0"],[2])
+
+    os.chdir("/")
+    folder = sys.argv[0].replace("\\","/")
+    folder = folder[:re.search("VLC_player",folder).end()]+"/"
+    os.chdir(folder)
+
+    def windows_part(files):
+        for i in range(len(files)):
+            try:
+                os.system(f'start /b cmd /k "%PROGRAMFILES%/VideoLAN/VLC/vlc.exe" {files[i]}')
+                time.sleep(0.5)
+                hold_and_press(["alt","tab"],[])
+                pyautogui.hotkey("alt","f4")
+                
+                image=pyscreenshot.grab(bbox=(0, 0, 300, 45))
+                time.sleep(2)
+                image.save(f'blank.png')         
+                time.sleep(3) 
+                while True:
+                    pos = imagesearch(f'blank.png')
+                    print(pos)
+                    if pos == [-1,-1]:
+                        break
+
+                pyautogui.hotkey("alt","f4")
+                
+            except FileNotFoundError:
+                pass
+
+    def linux_part(files):
+        for i in range(len(files)):
+            try:             
+                subprocess.run(["vlc",files[i]])
+                
+                image=pyscreenshot.grab(bbox=(210, 0, 350, 45))
+                image=pyscreenshot.grab(bbox=(0, 0, 300, 45))
+                time.sleep(2)
+                image.save(f'blank.png')         
+                time.sleep(3) 
+                while True:
+                    pos = imagesearch(f'blank.png')
+                    if pos == [-1,-1]:
+                        break
+
+                pyautogui.hotkey("alt","f4")
+
+            except FileNotFoundError:
+                pass
+
+    if loops == True:
+        if platform.system() == "Windows":
+            if loops == True:
+                windows_part(files)
+            else:
+                while True:
+                    windows_part(files)
+        
+    elif platform.system() == "Linux":
+        if loops == True:
+            linux_part(files)
+        else:
+            while True:
+                linux_part(files)
+
+
+    else:
+        raise OSError("OS {} is not supported.".format(platform.system()))
+
+    os.remove('blank.png')
+
+    exit(0)

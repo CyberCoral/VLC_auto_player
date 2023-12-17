763ac5c8d6c4311aad215c8263e768b668512688
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2023-12-17 18:46:31 +0100

    Create any_auto_player.sh
    Create a bash shortcut for the new any_auto_player function on VLC_auto_player.py

---
+++ VLC_player/any_auto_player.sh
@@ -0,0 +1,40 @@
+case "$1" in 
+    "true")
+    loop="True"
+    ;;
+    "false")
+    loop="False"
+    ;;
+    *)
+    echo "$1 not in [true, false]"
+    exit 1
+    ;;
+esac
+
+case "$2" in 
+    "true")
+    txt_="True"
+    ;;
+    "false")
+    txt_="False"
+    ;;
+    *)
+    echo "$2 not in [true, false]"
+    exit 1
+    ;;
+esac
+
+files=("${@:3:$#}")
+file=("'${files[0]}'")
+
+len=${#files[@]}
+
+for (( i=1;$i<$len ;i++ )); do
+    file="${file[@]},'${files[i]}'"
+done
+
+echo "import VLC_auto_player">>reproduce_file_VLC.py
+echo "from VLC_auto_player import any_auto_player as VLC">>reproduce_file_VLC.py
+echo "VLC($loop,False,$txt_,[${file[@]}])">>reproduce_file_VLC.py
+python -m reproduce_file_VLC
+rm reproduce_file_VLC.py
